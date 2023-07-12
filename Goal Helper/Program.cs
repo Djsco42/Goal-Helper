2fe921d74fc6d9727d5dc495b9efd2da1461b296
Dylan Scofield <djscofield42@gmail.com>
2023-07-11 18:27:41 -0700

    Initial commit

---
+++ Goal Helper/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Goal_Helper
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            Thread thread = new Thread(() =>
+            {
+                var app = new System.Windows.Application();
+                var mainWindow = new GoalWin.MainWindow(); // Replace "YourWpfApp" with the namespace of your WPF app
+                app.Run(mainWindow);
+               
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            
+
+        }
+    }
+}
